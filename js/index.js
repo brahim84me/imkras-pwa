c6c314da16b814cf84082318c149538f25ecf657
brahim84me <133565850+brahim84me@users.noreply.github.com>
2026-09-05 13:16:17 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// Imkras Cordova bootstrap
+// The application UI and logic are contained in index.html.
+document.addEventListener('deviceready', function () {
+  console.log('Imkras Cordova is ready');
+}, false);
